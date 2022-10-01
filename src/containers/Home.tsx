6f2dc3021348c453a522ee256f6991838bfd2f0b
allivent <allivent@gmail.com>
2022-10-01 18:30:50 -0400

    dynamic course links

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,14 +1,23 @@
 import { Link } from 'react-router-dom';
+import AllCourses from '../data/AllCourses';
+
+function renderCourseLinks() {
+  const courses = Object.values(AllCourses);
+  return courses.map((course, index) => {
+    return (
+      <span key={`course-${course.id}`}>
+        <Link to={`/${course.id}`}>{course.name}</Link>
+        {index < courses.length - 1 ? ' | ' : null}
+      </span>
+    );
+  });
+}
 
 function Home() {
   return (
     <div className="App">
       <h1>Sudachi</h1>
-      <div className="card">
-        <Link to="/genki1">Genki 1</Link>
-        {' | '}
-        <Link to="/jfbp1">Japanese for Busy People 1</Link>
-      </div>
+      <div className="card">{renderCourseLinks()}</div>
     </div>
   );
 }
